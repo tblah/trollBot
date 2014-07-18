8bd59277b389c5106cfec29f7c5b5d0e9d39b2fd
Tom Eccles <t@freedommail.info>
2014-07-18 20:59:06 +0100

    Messages are now specified in the configuration file.
    The code is very simple and explanitory.

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ trollBot
 A twisted IRC bot written in python. Hopefully this will end up being very easy to use and will support TLS. This is GPL v3 licenced.
 
 # Usage:
-You will need python 2.7 and the twisted networking stuff. See https://twistedmatrix.com/trac/.
+You will need python 2.7, PyYAML and the twisted networking stuff (including components for TLS). See https://twistedmatrix.com/trac/.
 
-Copy trollBot.yaml.example to trollBot.yaml and edit it to include the login details of the IRC server and channel which you would like to use.
+Copy trollBot.yaml.example to trollBot.yaml and edit it to include the login details of the IRC server and channel which you would like to use. Also specify the regular expressions used for matching messages and the response texts. Two examples have been included already.
 
 Finally run trollBot.py.
--- trollbot.py
+++ trollbot.py
@@ -32,16 +32,10 @@ class trollBot(irc.IRCClient):
 
     def privmsg(self, user, channel, msg):			###### THIS IS THE INTERESTING ONE!
         # got a message
-        linuxRegex = '(\s|^)[lL]inux'
-        ossRegex = '[oO]pen\s[sS]ource'
-
-        matches = re.search(linuxRegex, msg)
-        if matches:
-            self.reply(channel, "I would like to interject for a moment. What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\n\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called 'Linux', and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\n\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called 'Linux' distributions are really distributions of GNU/Linux.")
-
-        matches = re.search(ossRegex, msg)
-        if matches:
-            self.reply(channel, "It looks like you need to read https://gnu.org/philosophy/open-source-misses-the-point.html. Open source is a development model, saying this is does not recomend the political importance of Free Software.")
+        for regex in config['regex']:
+            match = re.search(regex, msg)
+            if match:
+                self.reply(channel, config['regex'][regex])
 
     def reply(self, channel, message):
     	# a wrapper for self.say() that makes sure we are utf-8
